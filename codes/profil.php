3e2a6b46055ba12d37f4393de2e6647cea67add1
Senami16 <glfyann@gmail.com>
2025-01-31 10:58:17 +0100

    page de configuration

--- codes/profil.php
+++ codes/profil.php
@@ -134,7 +134,7 @@ foreach ($appointments as $appointment) {
                 <div class="collapse navbar-collapse">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item"><a class="nav-link" href="modif_infos.php">Mon espace</a></li>
-                        <li class="nav-item"><a class="nav-link" href="gestion_mesures.html">Mes patients</a></li>
+                        <li class="nav-item"><a class="nav-link" href="gestion_mesures.php">Mes patients</a></li>
                         <li class="nav-item"><a class="nav-link" href="configuration.html">Configuration</a></li>
                     </ul>
                 </div>
